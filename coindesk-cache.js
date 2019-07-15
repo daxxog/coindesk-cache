3ceb76b9efe17a1f3908ecda3ed5d2ed41d1a687
David Volm <david@volminator.com>
2019-07-15 14:16:09 -0500

    fixed a nasty scope bug

--- coindesk-cache.js
+++ coindesk-cache.js
@@ -96,174 +96,12 @@
 
                         apiEndpoints.forEach(function(__endpoint) {
                             var _endpoint = __endpoint.replace("[!supported]", "*"),
-                                endpointSpecial = !(_endpoint === __endpoint),
-                                endpointGet = {};
+                                endpointSpecial = !(_endpoint === __endpoint);
 
-                            app.get(apiHeader + _endpoint, function(req, res) {
-                                var query = {
-                                    endpoint: req.path,
-                                    date: (new Date()).toDateString()
-                                };
-
-                                var remoteEndpoint = argv.endpoint.replace(apiHeader, req.path),
-                                    canRun = true,
-                                    failReason = false;
-
-                                var resJson = function(obj) {
-                                    if(req.path === (apiHeader + "supported-currencies.json")) {
-                                        if(!isSupported("EUR")) { //if we are using a dummy function
-                                            isSupported = sf.is(obj.map(function(v) { //create a real function
-                                                return v.currency;
-                                            }));
-                                        }
-                                    }
-
-                                    res.json(obj);
-                                };
-
-                                if(endpointSpecial) {
-                                    var currency = path.posix.basename(req.path).replace(".json", "");
-                                    canRun = isSupported(currency);
-
-                                    if(!canRun) {
-                                        failReason = "unsupported-currency";
-                                    }
-                                }
-
-                                if(req.path === (apiHeader + "historical/close.json")) {
-                                    if(typeof req.query.index !== "undefined") {
-                                        if(isValidIndex(req.query.index)) {
-                                            endpointGet.index = req.query.index;
-                                        } else {
-                                            canRun = false;
-                                            failReason = "unsupported-index";
-                                        }
-                                    }
-
-                                    if(typeof req.query.currency !== "undefined") {
-                                        if(isSupported(req.query.currency)) {
-                                            endpointGet.index = req.query.currency;
-                                        } else {
-                                            canRun = false;
-                                            failReason = "unsupported-currency";
-                                        }
-                                    }
-
-                                    if(typeof req.query.for !== "undefined") {
-                                        if(req.query.for === "yesterday") {
-                                            endpointGet.for = req.query.for;
-                                        } else {
-                                            canRun = false;
-                                            failReason = "for-yesterday";
-                                        }
-                                    }
-
-                                    if((typeof req.query.start !== "undefined") || (typeof req.query.end !== "undefined")) { //has atleast a start or an end
-                                        if((typeof req.query.start !== "undefined") && (typeof req.query.end !== "undefined")) { //has both a start and an end
-                                            if(CoindeskCache.validDateString(req.query.start)) {
-                                                if(CoindeskCache.validDateString(req.query.end)) {
-                                                    if(moment(req.query.start).format('X') <= moment(req.query.end).format('X')) {
-                                                        endpointGet.start = req.query.start;
-                                                        endpointGet.end = req.query.end;
-                                                    } else {
-                                                        canRun = false;
-                                                        failReason = "start-gt-end";
-                                                    }
-                                                } else {
-                                                    canRun = false;
-                                                    failReason = "invalid-date-end";
-                                                }
-                                            } else {
-                                                canRun = false;
-                                                failReason = "invalid-date-start";
-                                            }
-                                        } else {
-                                            canRun = false;
-                                            failReason = "need-both";
-                                        }
-                                    }
-
-                                    for(var key in endpointGet) { //safe to copy over to the mongodb query
-                                        query[key] = endpointGet[key];
-                                    }
-                                }
-
-                                if(canRun) {
-                                    collection.countDocuments(query, {}, function(err, count) {
-                                        if(!err) {
-                                            if(count === 0) {
-                                                console.log("Connecting to endpoint: " + remoteEndpoint);
-                                                request({
-                                                    uri: remoteEndpoint,
-                                                    qs: endpointGet
-                                                }, function(err, response, body) {
-                                                    if(!err) {
-                                                        var parsed = JSON.parse(body),
-                                                            insert = {
-                                                                endpoint: req.path,
-                                                                date: (new Date()).toDateString(),
-                                                                body: parsed
-                                                            };
-
-                                                        for(var key in endpointGet) { //safe to copy over to the mongodb insert
-                                                            insert[key] = endpointGet[key];
-                                                        }
-
-                                                        collection.insertOne(insert, {}, ErrorHandler);
-                                                        
-                                                        resJson(parsed);
-                                                    } else {
-                                                        ErrorHandler(err);
-                                                    }
-                                                });
-                                            } else {
-                                                collection.findOne(query, {}, function(err, doc) {
-                                                    if(!err) {
-                                                        resJson(doc.body);
-                                                    } else {
-                                                        ErrorHandler(err);
-                                                    }
-                                                });
-                                            }
-                                        } else {
-                                            ErrorHandler(err);
-                                        }
-                                    });
-                                } else {
-                                    switch(failReason) {
-                                        case "unsupported-currency":
-                                            resJson("Sorry, your requested currency is not supported or is invalid. Please check supported-currencies.json for a list of supported currencies.");
-                                            
-                                            break;
-                                        case "unsupported-index":
-                                            resJson("Invalid index value. Support index values are " + JSON.stringify(supportedIndex));
-                                            
-                                            break;
-                                        case "for-yesterday":
-                                            resJson('for must be "yesterday"');
-                                            
-                                            break;
-                                        case "need-both":
-                                            resJson('"start" and "end" queries need both a start and an end');
-                                            
-                                            break;
-                                        case "invalid-date-start":
-                                            resJson('invalid date. "start" query needs to be in the format YYYY-MM-DD. the CoinDesk BPI only covers data from 2010-07-17 onwards.');
-                                            
-                                            break;
-                                        case "invalid-date-end":
-                                            resJson('invalid date. "end" query needs to be in the format YYYY-MM-DD. the CoinDesk BPI only covers data from 2010-07-17 onwards.');
-                                            
-                                            break;
-                                        case "start-gt-end":
-                                            resJson('invalid date. "start" date must be before (less than or equal to) the "end" date');
-                                            
-                                            break;
-                                        default: //should never happen..
-                                            resJson("an unexpected error occurred!");
-                                    }
-                                }
-                            });
+                            app.get(apiHeader + _endpoint, (new CoindeskCache.Middleware({
+                                endpointSpecial: endpointSpecial,
+                                collection: collection
+                            })).factory());
                         }); //end of dynamic express app logic
                         
                         /* paste this code into dev console on
@@ -324,6 +162,203 @@
             }
         } else {
             return false;
+        }
+    };
+
+    //created a Middleware class to hold most of the logic
+    //helped me discover a nasty scope based bug
+
+    CoindeskCache.Middleware = function(obj) {
+        for(var key in obj) {
+            this[key] = obj[key];
+        }
+    };
+
+    CoindeskCache.Middleware.prototype.factory = function() {
+        var that = this;
+
+        return function(req, res) {
+            that.run(req, res, that);
+        };
+    };
+
+    CoindeskCache.Middleware.prototype.run = function(req, res, that) {
+        var query = {
+            endpoint: req.path,
+            date: (new Date()).toDateString()
+        };
+
+        var remoteEndpoint = argv.endpoint.replace(apiHeader, req.path),
+            canRun = true,
+            failReason = false,
+            endpointGetLocal = {};
+
+        var resJson = function(obj) {
+            if(req.path === (apiHeader + "supported-currencies.json")) {
+                if(!isSupported("EUR")) { //if we are using a dummy function
+                    isSupported = sf.is(obj.map(function(v) { //create a real function
+                        return v.currency;
+                    }));
+                }
+            }
+
+            res.json(obj);
+        };
+
+        if(that.endpointSpecial) {
+            var currency = path.posix.basename(req.path).replace(".json", "");
+            canRun = isSupported(currency);
+
+            if(!canRun) {
+                failReason = "unsupported-currency";
+            }
+        }
+
+        if(req.path === (apiHeader + "historical/close.json")) {
+            if(typeof req.query.index !== "undefined") {
+                if(isValidIndex(req.query.index)) {
+                    endpointGetLocal.index = req.query.index;
+                } else {
+                    canRun = false;
+                    failReason = "unsupported-index";
+                }
+            }
+
+            if(typeof req.query.currency !== "undefined") {
+                if(isSupported(req.query.currency)) {
+                    endpointGetLocal.index = req.query.currency;
+                } else {
+                    canRun = false;
+                    failReason = "unsupported-currency";
+                }
+            }
+
+            if(typeof req.query.for !== "undefined") {
+                if(req.query.for === "yesterday") {
+                    endpointGetLocal.for = req.query.for;
+                } else {
+                    canRun = false;
+                    failReason = "for-yesterday";
+                }
+            }
+
+            if((typeof req.query.start !== "undefined") || (typeof req.query.end !== "undefined")) { //has atleast a start or an end
+                if((typeof req.query.start !== "undefined") && (typeof req.query.end !== "undefined")) { //has both a start and an end
+                    if(CoindeskCache.validDateString(req.query.start)) {
+                        if(CoindeskCache.validDateString(req.query.end)) {
+                            if(moment(req.query.start).format('X') <= moment(req.query.end).format('X')) {
+                                endpointGetLocal.start = req.query.start;
+                                endpointGetLocal.end = req.query.end;
+                            } else {
+                                canRun = false;
+                                failReason = "start-gt-end";
+                            }
+                        } else {
+                            canRun = false;
+                            failReason = "invalid-date-end";
+                        }
+                    } else {
+                        canRun = false;
+                        failReason = "invalid-date-start";
+                    }
+                } else {
+                    canRun = false;
+                    failReason = "need-both";
+                }
+            }
+
+            for(var key in endpointGetLocal) { //safe to copy over to the mongodb query
+                query[key] = endpointGetLocal[key];
+            }
+        }
+
+        if(canRun) {
+           that.collection.findOne(query, {}, function(err, doc) {
+                if(!err) {
+                    if(doc === null) {
+                        /* DEBUG 
+                        console.log("Connecting to endpoint: " + remoteEndpoint);
+                        console.log("for query: ");
+                        console.log(query);
+                        */
+
+                        request({
+                            uri: remoteEndpoint,
+                            qs: endpointGetLocal
+                        }, function(err, response, body) {
+                            if(!err) {
+                                var parsed = JSON.parse(body),
+                                    insert = {
+                                        endpoint: req.path,
+                                        date: (new Date()).toDateString(),
+                                        body: parsed
+                                    };
+
+                                for(var key in endpointGetLocal) { //safe to copy over to the mongodb insert
+                                    insert[key] = endpointGetLocal[key];
+                                }
+
+                                var upsert = {
+                                    "$set": insert
+                                };
+
+                                /* DEBUG 
+                                console.log("upsert data");
+                                console.log(upsert);
+                                console.log(query);
+                                */
+
+                                //collection.insertOne(insert, {}, ErrorHandler);
+                                that.collection.updateOne(query, upsert, {
+                                    upsert: true
+                                }, ErrorHandler);
+
+
+                                resJson(parsed);
+                            } else {
+                                ErrorHandler(err);
+                            }
+                        });
+                    } else {
+                        resJson(doc.body);
+                    }
+                } else {
+                    ErrorHandler(err);
+                }
+            });
+        } else {
+            switch(failReason) {
+                case "unsupported-currency":
+                    resJson("Sorry, your requested currency is not supported or is invalid. Please check supported-currencies.json for a list of supported currencies.");
+                    
+                    break;
+                case "unsupported-index":
+                    resJson("Invalid index value. Support index values are " + JSON.stringify(supportedIndex));
+                    
+                    break;
+                case "for-yesterday":
+                    resJson('for must be "yesterday"');
+                    
+                    break;
+                case "need-both":
+                    resJson('"start" and "end" queries need both a start and an end');
+                    
+                    break;
+                case "invalid-date-start":
+                    resJson('invalid date. "start" query needs to be in the format YYYY-MM-DD. the CoinDesk BPI only covers data from 2010-07-17 onwards.');
+                    
+                    break;
+                case "invalid-date-end":
+                    resJson('invalid date. "end" query needs to be in the format YYYY-MM-DD. the CoinDesk BPI only covers data from 2010-07-17 onwards.');
+                    
+                    break;
+                case "start-gt-end":
+                    resJson('invalid date. "start" date must be before (less than or equal to) the "end" date');
+                    
+                    break;
+                default: //should never happen..
+                    resJson("an unexpected error occurred!");
+            }
         }
     };
 
